8d35d5710625ab690c90464c4c8dac07916cad3f
vijayrana05 <162808238+vijayrana05@users.noreply.github.com>
2024-07-07 15:27:02 +0000

    dataTypes_summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,21 @@
+const num = 3893
+const str = "vijay rana"
+const temp  = null
+const carName = undefined
+console.log(typeof num)
+console.log(typeof str)
+console.log(typeof temp)
+console.log(typeof carName)
+
+
+const myFunction = function() {
+    console.log("this is a funciton")
+}
+console.log(typeof myFunction)
+// console.log(typeof function) error
+
+// +++++++++++++++++++++++++++++++++++++++++
+const name = "vijay"
+const standard =  12
+console.log(`my name is ${name} from class ${standard}`)
+
