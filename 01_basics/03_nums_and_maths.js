c82f23b0bd846f5d3829509909af504ba52013b1
vijayrana05 <162808238+vijayrana05@users.noreply.github.com>
2024-07-15 08:46:25 +0000

    nums and maths

---
+++ 01_basics/03_nums_and_maths.js
@@ -0,0 +1,17 @@
+const n  = 300.4884;
+
+console.log(n.toFixed(2)) // round of to two decimal places
+
+const balance  = new Number(900)
+console.log(balance)
+console.log(n)
+
+const x = 122.86
+console.log(x.toPrecision(4))
+
+console.log(Math.abs(-7)) // negative to pos but pos remains pos
+console.log(Math.ceil(4.3)) // gives 5
+console.log(Math.floor(4.3)) // gives 4
+console.log(Math.round(4.5)) // gives 5 for values equal to graeter than 4.5
+
+
